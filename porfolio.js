b3f3f788d38832186c675f8613a5535c25353967
Senuki Perera <109462298+SenukiPerera@users.noreply.github.com>
2023-11-25 17:55:33 +0530

    Add files via upload

---
+++ porfolio.js
@@ -0,0 +1,12 @@
+let type = new Typed('#changing-text', {
+    strings:['Front-End Developer', 'UI Designer', 'Web Developer'],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true,
+})
+
+/*window.addEventListener("scroll", function(){
+    var header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY = 0)
+})*/
